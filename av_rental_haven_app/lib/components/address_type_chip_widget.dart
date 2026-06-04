3ec3b78db84dbe6bf19c2d6ab30cf858053caad0
spicoding <shaneinduli@gmail.com>
2026-06-04 14:39:48 +0300

    Unused Import Removals

--- av_rental_haven_app/lib/components/address_type_chip_widget.dart
+++ av_rental_haven_app/lib/components/address_type_chip_widget.dart
@@ -1,7 +1,5 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 
 import '../models/address_type_chip_model.dart';
 
